f3fde5cfd7b2fcf063aa440dec1fd967794a5116
jacktonmulagi <otingajacton@gmail.com>
2023-01-17 12:00:00 +0300

    changes

--- src/main/java/com/mulagiHub/DailyRevenueSummaryTelegramBot/service/RevenueSummary.java
+++ src/main/java/com/mulagiHub/DailyRevenueSummaryTelegramBot/service/RevenueSummary.java
@@ -34,24 +34,24 @@ public class RevenueSummary {
         LocalDate now = LocalDate.now();
         List<SumRevenue> revenues = new ArrayList<>();
         if(Objects.equals(database, "africom")){
-            summary.add(0,"Revenue Summary of Africom media limited on " + now.minus(Period.ofDays(1)) );
+            summary.add(0,"Revenue Summary of Africom media limited on " + now.minus(Period.ofDays(1)) + System.lineSeparator() );
              revenues = africomRevenueHandler.findAllRevenue(String.valueOf(now.minus(Period.ofDays(1))));
         } else if (Objects.equals(database, "breaktime")) {
-            summary.add(0,"Revenue Summary of Breaktime  on " + now.minus(Period.ofDays(1)));
+            summary.add(0,"Revenue Summary of Breaktime  on " + now.minus(Period.ofDays(1)) + System.lineSeparator() );
             revenues = breakTimeRevenueHandler.findAllRevenue(String.valueOf(now.minus(Period.ofDays(1))));
         }
         else if (Objects.equals(database, "tajbuzz")) {
-            summary.add(0,"Revenue Summary of Tajbuzz on " + now.minus(Period.ofDays(1)));
+            summary.add(0,"Revenue Summary of Tajbuzz on " + now.minus(Period.ofDays(1)) + System.lineSeparator() );
             revenues = tajBuzzRevenueHandler.findAllRevenue(String.valueOf(now.minus(Period.ofDays(1))));
         }
         else if (Objects.equals(database, "zuree")) {
-            summary.add(0,"Revenue Summary of Zuree on " + now.minus(Period.ofDays(1)));
+            summary.add(0,"Revenue Summary of Zuree on " + now.minus(Period.ofDays(1)) + System.lineSeparator() );
             revenues = zureeRevenueHandler.findAllRevenue(String.valueOf(now.minus(Period.ofDays(1))));
         }
 
         for (SumRevenue revenue : revenues) {
             int position = revenues.indexOf(revenue)+1 ;
-            summary.add("No." + position +" "+ revenue.getPartnerName() + " ==>  ksh " + revenue.getAmount() );
+            summary.add("No." + position +" "+ revenue.getPartnerName() + " ==>  ksh " + revenue.getAmount() + System.lineSeparator() );
 
         }
         return summary.toString().replaceAll("\\[|\\]", "");
